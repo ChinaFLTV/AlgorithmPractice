47a05c1fec780fc54283e37edfe6e6852f6b9001
ChinaFLTV <236010069@qq.com>
2024-06-08 23:36:02 +0800

    2024-6-8  23:35-day75

--- src/main/kotlin/day75/ReverseWordsInAString.kt
+++ src/main/kotlin/day75/ReverseWordsInAString.kt
@@ -22,11 +22,138 @@ package day75
 class Solution {
 
 
+    private val words = ArrayList<String>()
+
+
     fun reverseWords(s: String): String {
 
+        val words = s.split(Regex("\\s+"))
+            .filter { it.isNotEmpty() }
+            .reversed()
+            .toTypedArray()
 
+        return words.joinToString(" ")
 
     }
 
+
+    fun reverseWords_answer_1(s: String): String {
+
+        var need = false
+        var left = -1
+        var right = 0
+        val res = StringBuilder("")
+
+        var i = 0
+        while (i < s.length) {
+
+            if (s[i] == ' ') {
+
+                if (need) {
+
+                    while (s[i] != ' ') {
+
+                        i++
+
+                    }
+                    res.append(' ')
+                    --i
+
+                }
+
+            } else {
+
+                if (!need) {
+
+                    need = true
+
+                }
+
+                if (left == null) {
+
+                    left = i
+
+                }
+
+
+                while (s[i] != ' ') {
+
+                    i++
+
+                }
+
+                --i
+                right = i
+                swap(s, res, left, right)
+
+            }
+
+
+        }
+
+        return ""
+
+    }
+
+
+    private fun swap(s: String, res: StringBuilder, left: Int, right: Int) {
+
+        for (i in right..left) {
+
+            res.append(s[i])
+
+        }
+
+    }
+
+
+    fun reverseWords_answer_2(s: String): String {
+
+        val res = StringBuilder("")
+
+        var i = s.length - 1
+        var shouldAdd = false
+
+        while (i >= 0) {
+
+            if (s[i] != ' ') {
+
+                if (shouldAdd)
+                    res.append(' ')
+
+                val right = i
+                while (i >= 0 && s[i] != ' ') {
+
+                    i--
+
+                }
+
+                for (j in i + 1..right) {
+
+                    res.append(s[j])
+
+                }
+
+                shouldAdd = true
+
+            } else {
+
+                i--
+
+            }
+
+        }
+
+        return res.toString()
+
+    }
+
+
+}
+
+
+fun main() {
+
+    println(Solution().reverseWords_answer_2("the sky is blue"))
 
 }
